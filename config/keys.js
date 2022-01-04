4e8edd9ab2a896d0a5d4d11ad16453328a8bf016
John OBrien <johnobrien8642@gmail.com>
2022-01-04 09:05:20 -0800

    try port 80

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 const keys = {
-  url: process.env.NODE_ENV === 'development' ?  `http://localhost:${process.env.PORT}` : `http://localhost:${process.env.VERCEL_PORT}`
+  url: process.env.NODE_ENV === 'development' ?  `http://localhost:${process.env.PORT}` : `http://localhost:80`
 }
 
 export default keys
