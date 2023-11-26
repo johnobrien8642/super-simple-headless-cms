ab9c3df5e691031998cdcb425e8d4e589e58033f
John O'Brien <62157096+johnobrien8642@users.noreply.github.com>
2023-11-25 21:30:34 -0500

    fix assets import

--- pages/api/get_video.js
+++ pages/api/get_video.js
@@ -1,12 +1,12 @@
 import connectDb from '../../lib/mongodb.js';
-import Asset from '../../models/Asset';
+import Assets from '../../models/Assets';
 
 export default async (req, res) => {
 	await connectDb();
 	const { _id } = req.body;
 
 	try {
-		const asset = await Asset.find({ _id });
+		const asset = await Assets.find({ _id });
 		res.status(200).json({ success: true, video: asset });
 	} catch (err) {
 		res.status(500).json({ success: false, errorMessage: err.message });
