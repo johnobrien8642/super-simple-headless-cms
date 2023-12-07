9181e1364a5976d2f6cedc3a86f49a73a52acf3a
John O'Brien <62157096+johnobrien8642@users.noreply.github.com>
2023-12-07 04:18:27 -0500

    try model-types, try Assets model with type obj

--- models/Assets.ts
+++ models/Assets.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 import { assetTypes, textAlignOptions } from '../template_options';
+import { OptionsType, assetsEnumValueArr, textAlignOptionsEnumValueArr } from './model-types';
 const Schema = mongoose.Schema;
 
-const AssetsSchema = new Schema({
+const optionsObj: { [key: string]: OptionsType } = {
 	assetKey: {
-		type: String,
 		file: true,
 		formTitle: 'Asset File',
 		dataFormKey: 'assetFile',
@@ -13,19 +13,17 @@ const AssetsSchema = new Schema({
 		previewTypeKey: 'assetPreviewType',
 		index: true,
 		templates: {
-			'Image Grid': 1,
-			'Image Triptych': 1,
-			'Photo List': 1,
-			'Book Cover CTA': 1,
+			'ImageGrid': 1,
+			'ImageTriptych': 1,
+			'PhotoList': 1,
+			'BookCoverCTA': 1,
 			'About': 1
 		}
 	},
 	assetDimensions: {
-		type: [Number, Number],
 		hide: true
 	},
 	thumbnailKey: {
-		type: String,
 		file: true,
 		formTitle: 'Thumbnail File',
 		dataFormKey: 'thumbnailFile',
@@ -38,91 +36,139 @@ const AssetsSchema = new Schema({
 		}
 	},
 	thumbnailDimensions: {
-		type: [Number, Number],
 		hide: true
 	},
 	base64String: {
-		type: Buffer,
 		hide: true
 	},
 	title: {
-		type: String,
 		templates: {
-			'Image Grid': 1,
-			'Image Triptych': 1,
-			'Photo List': 1,
-			'Book Cover CTA': 1,
-			'Headline Only CTA': 1,
-			'Text Block': 1
+			'ImageGrid': 1,
+			'ImageTriptych': 1,
+			'PhotoList': 1,
+			'BookCoverCTA': 1,
+			'HeadlineOnlyCTA': 1,
+			'TextBlock': 1
 		}
 	},
 	type: {
-		type: String,
-		enum: assetTypes,
+		enum: assetsEnumValueArr,
 		required: true,
 		select: true,
 		enumKey: 'assetTypes'
 	},
 	description: {
-		type: String,
 		textbox: true,
 		formTitle: 'Regular Text',
 		templates: {
-			'Image Triptych': 1,
-			'Book Cover CTA': 1,
+			'ImageTriptych': 1,
+			'BookCoverCTA': 1,
 			'About': 1
 		}
 	},
 	textAlign: {
-		type: String,
-		enum: textAlignOptions,
+		enum: textAlignOptionsEnumValueArr,
 		select: true,
 		enumKey: 'textAlignOptions',
 		formTitle: 'Rich Text and Title Align',
 		defaultValue: 'left',
 		templates: {
-			'Text Block': 1
+			'TextBlock': 1
 		}
 	},
 	richDescription: {
-		type: String,
 		richText: true,
 		formTitle: 'Rich Text',
 		templates: {
-			'Image Grid': 1,
-			'Image Triptych': 1,
-			'Photo List': 1,
-			'Book Cover CTA': 1,
-			'Text Block': 1,
+			'ImageGrid': 1,
+			'ImageTriptych': 1,
+			'PhotoList': 1,
+			'BookCoverCTA': 1,
+			'TextBlock': 1,
 			'About': 1
 		}
 	},
 	extLink: {
-		type: String,
 		formTitle: 'External Link',
 		templates: {
-			'Image Grid': 1,
-			'Photo List': 1,
-			'Book Cover CTA': 1
+			'ImageGrid': 1,
+			'PhotoList': 1,
+			'BookCoverCTA': 1
 		}
 	},
 	schemaName: {
-		type: String,
 		default: 'Assets',
 		hide: true,
 		internal: true
 	},
 	isDuplicate: {
-		type: Boolean,
 		default: false,
 		hide: true,
 		internal: true
 	},
 	createdAt: {
-		type: Date,
-		default: Date.now,
 		hide: true,
 		internal: true
+	}
+}
+
+const AssetsSchema = new Schema({
+	assetKey: {
+		type: String,
+		...optionsObj.assetKey
+	},
+	assetDimensions: {
+		type: [Number, Number],
+		...optionsObj.assetDimensions
+	},
+	thumbnailKey: {
+		type: String,
+		...optionsObj.thumbnailKey
+	},
+	thumbnailDimensions: {
+		type: [Number, Number],
+		...optionsObj.thumbnailDimensions
+	},
+	base64String: {
+		type: Buffer,
+		...optionsObj.base64String
+	},
+	title: {
+		type: String,
+		...optionsObj.title
+	},
+	type: {
+		type: String,
+		...optionsObj.type
+	},
+	description: {
+		type: String,
+		...optionsObj.description
+	},
+	textAlign: {
+		type: String,
+		...optionsObj.textAlign
+	},
+	richDescription: {
+		type: String,
+		...optionsObj.richDescription
+	},
+	extLink: {
+		type: String,
+		...optionsObj.extLink
+	},
+	schemaName: {
+		type: String,
+		...optionsObj.schemaName
+	},
+	isDuplicate: {
+		type: Boolean,
+		...optionsObj.isDuplicate
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		...optionsObj.createdAt
 	}
 });
 
---
+++ models/model-types.ts
@@ -0,0 +1,85 @@
+import mongoose, { Schema, Types } from 'mongoose';
+typeof mongoose.SchemaTypes
+
+// ATTN: Enum key and value should match
+// template component name Pascal Cased.
+export enum TemplatesEnum {
+	ImageGrid = 'ImageGrid',
+	ImageTriptych = 'ImageTriptych',
+	PhotoList = 'PhotoList',
+	VideoPlayer = 'VideoPlayer',
+	BookCoverCTA = 'BookCoverCTA',
+	HeadlineOnlyCTA = 'HeadlineOnlyCTA',
+	TextBlock = 'TextBlock',
+	About = 'About',
+	PdfList = 'PdfList'
+}
+
+export enum AssetsEnum {
+	Image = 'Image',
+	Video = 'Video',
+	Text = 'Text',
+	PDF = 'PDF'
+}
+
+export enum TextAlignOptionsEnum {
+	Left = 'left',
+	Center = 'center',
+	Right = 'right',
+	None = ''
+}
+
+export type SubdocumentType = {
+	type: Types.ObjectId;
+	ref: string;
+}
+
+export const templatesEnumValueArr = Object.values(TemplatesEnum);
+export const assetsEnumValueArr = Object.values(AssetsEnum);
+export const textAlignOptionsEnumValueArr = Object.values(TextAlignOptionsEnum);
+
+export type OptionsType = {
+	// Can be multiple different kinds as per mongoose, handled by Typegoose ultimately
+	required?: boolean;
+	default?: boolean | string | number | Date;
+	index?: boolean;
+	enum?: typeof templatesEnumValueArr | typeof assetsEnumValueArr | typeof textAlignOptionsEnumValueArr;
+	// For accessing enum array from object (deprecated)
+	enumKey?: string;
+	// Default value for the select input
+	defaultValue?: string;
+	formTitle?: string;
+	// Hide this input from the form
+	hide?: boolean;
+	// Display a textarea input instead of regular text input in the form
+	textbox?: boolean;
+	// Use file input in the form
+	file?: boolean;
+	// For displaying a boolean toggle switch in the form
+	select?: boolean;
+	// For displaying a rich text editor in the form
+	richText?: boolean;
+	// For nested fields like "meta" in Page, dropdown button text in the form
+	collapseTitle?: string;
+	// Input is only for internal use, but could appear in the form and take input.
+	// For example, if you have data that you want to use in a template, but not show
+	// in the UI
+	internal?: boolean;
+	// For setting some data during user input, i.e. asset
+	// in Assets file in assetKey, internal/background
+	dataFormKey?: string;
+	// For setting some preview url during user input, i.e. asset preview
+	// in Assets file in assetKey, internal/background
+	dataPreviewUrl?: string;
+	// For setting some asset dimensions during user input,
+	// i.e. asset dimensions in Assets file in assetKey, internal/background
+	dimensionsKey?: string;
+	// For setting some preview type during user input,
+	// i.e. asset preview type key in Assets file in assetKey, internal/background
+	previewTypeKey?: string;
+	// For highlighting fields in use for a given template in blue,
+	// i.e. this field is used in ImageGrid, include it as { 'ImageGrid': 1 }
+	templates?: { [key in TemplatesEnum]?: 1; };
+	// For limiting subdocument choices to just a single document in the form
+	singleChoice?: boolean;
+}
