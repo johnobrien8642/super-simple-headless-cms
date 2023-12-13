fc9e2f975f3275091d8cc2daf45cb549b0e14012
John O'Brien <62157096+johnobrien8642@users.noreply.github.com>
2023-12-13 18:52:34 -0500

    inactivate repl 3

--- pages/auth/repl.tsx
+++ pages/auth/repl.tsx
@@ -59,90 +59,90 @@ const ReplWindow = ({}) => {
 
 	return <></>
 
-	if (process.env.NEXT_PUBLIC_SUPER_ACTIVE === 'false' || data) {
-		return (
-			<Center>
-				<Grid
-					gridTemplateColumns={'1.5fr 1fr'}
-					gap={'5%'}
-					width={'90%'}
-					mt={'5%'}
-				>
-					<Box>
-						<Text as='h3' textDecoration={'underline'}><Link href={'/admin'}>Admin Page</Link></Text>
-						<Text as='h3' textDecoration={'underline'} mb={10}><Link href={'/create_post'}>Create Post</Link></Text>
-						<Formik
-							initialValues={codeEditorInitVals}
-							onSubmit={async (values, actions) => {
-								const res = await fetch('/api/auth/repl', {
-									method: 'POST',
-									body: JSON.stringify({ codeString, admin })
-								})
-								const data = await res.json()
-								setResult(data.resultString)
-							}}
-						>
-							{props => (
-								<form
-									onSubmit={props.handleSubmit}
-								>
-									<AceEditor
-										style={{ width: '100%'}}
-										mode='javascript'
-										setCodeString={setCodeString}
-										name='codeString'
-										editorProps={{ $blockScrolling: true }}
-										enableBasicAutocompletion
-										fontSize={'1rem'}
-									/>
-									<Button type='submit' mt={'5%'}>Submit</Button>
-									<Text as='h3' mt={50}>Personal Site Repl</Text>
-									<Text>-"models", "admin", and "dbConnect" available in repl function scope</Text>
-									<Text>-"models" is the Mongoose models index object, "admin" is the current signed in admin object</Text>
-									<Text>-"dbConnect" is the mongoose connection function, run at top of file like "await dbConnect()"</Text>
-									<Text>-Hit submit, code will be sent and run in API, results will be returned</Text>
-									<Text>Examples</Text>
-									<Text>- See available models</Text>
-									<Code children="return Object.keys(models)" />
-									<Text marginTop={5}>- Query all docs</Text>
-									<Flex
-										flexDirection={'column'}
-										width='fit-content'
-									>
-										<Code children="await dbConnect()" />
-										<Code children="const { Admin } = models" />
-										<Code children="return await Admin.find({})" />
-									</Flex>
-									<Text marginTop={5}>- Query all docs and iterate and return an array of ids</Text>
-									<Flex
-										flexDirection={'column'}
-										width='fit-content'
-										marginBottom={5}
-									>
-										<Code children="await dbConnect()" />
-										<Code children="const { Admin } = models" />
-										<Code children="const admins = await Admin.find({})" />
-										<Code children="return adminIds = admins.map(obj => obj._id)" />
-									</Flex>
-								</form>
-							)}
-						</Formik>
-					</Box>
-					<Box>
-						<Text as='h3'>Results</Text>
-						<Box
-							width={'100%'}
-							height={'100%'}
-							backgroundColor={'var(--chakra-colors-blackAlpha-200)'}
-							overflowY={'auto'}
-							padding={'2%'}
-						>{result}</Box>
-						<Button mt={'2%'} onClick={() => setResult('')}>Clear</Button>
-					</Box>
-				</ Grid>
-			</Center>
-		)
-	}
+	// if (process.env.NEXT_PUBLIC_SUPER_ACTIVE === 'false' || data) {
+	// 	return (
+	// 		<Center>
+	// 			<Grid
+	// 				gridTemplateColumns={'1.5fr 1fr'}
+	// 				gap={'5%'}
+	// 				width={'90%'}
+	// 				mt={'5%'}
+	// 			>
+	// 				<Box>
+	// 					<Text as='h3' textDecoration={'underline'}><Link href={'/admin'}>Admin Page</Link></Text>
+	// 					<Text as='h3' textDecoration={'underline'} mb={10}><Link href={'/create_post'}>Create Post</Link></Text>
+	// 					<Formik
+	// 						initialValues={codeEditorInitVals}
+	// 						onSubmit={async (values, actions) => {
+	// 							const res = await fetch('/api/auth/repl', {
+	// 								method: 'POST',
+	// 								body: JSON.stringify({ codeString, admin })
+	// 							})
+	// 							const data = await res.json()
+	// 							setResult(data.resultString)
+	// 						}}
+	// 					>
+	// 						{props => (
+	// 							<form
+	// 								onSubmit={props.handleSubmit}
+	// 							>
+	// 								<AceEditor
+	// 									style={{ width: '100%'}}
+	// 									mode='javascript'
+	// 									setCodeString={setCodeString}
+	// 									name='codeString'
+	// 									editorProps={{ $blockScrolling: true }}
+	// 									enableBasicAutocompletion
+	// 									fontSize={'1rem'}
+	// 								/>
+	// 								<Button type='submit' mt={'5%'}>Submit</Button>
+	// 								<Text as='h3' mt={50}>Personal Site Repl</Text>
+	// 								<Text>-"models", "admin", and "dbConnect" available in repl function scope</Text>
+	// 								<Text>-"models" is the Mongoose models index object, "admin" is the current signed in admin object</Text>
+	// 								<Text>-"dbConnect" is the mongoose connection function, run at top of file like "await dbConnect()"</Text>
+	// 								<Text>-Hit submit, code will be sent and run in API, results will be returned</Text>
+	// 								<Text>Examples</Text>
+	// 								<Text>- See available models</Text>
+	// 								<Code children="return Object.keys(models)" />
+	// 								<Text marginTop={5}>- Query all docs</Text>
+	// 								<Flex
+	// 									flexDirection={'column'}
+	// 									width='fit-content'
+	// 								>
+	// 									<Code children="await dbConnect()" />
+	// 									<Code children="const { Admin } = models" />
+	// 									<Code children="return await Admin.find({})" />
+	// 								</Flex>
+	// 								<Text marginTop={5}>- Query all docs and iterate and return an array of ids</Text>
+	// 								<Flex
+	// 									flexDirection={'column'}
+	// 									width='fit-content'
+	// 									marginBottom={5}
+	// 								>
+	// 									<Code children="await dbConnect()" />
+	// 									<Code children="const { Admin } = models" />
+	// 									<Code children="const admins = await Admin.find({})" />
+	// 									<Code children="return adminIds = admins.map(obj => obj._id)" />
+	// 								</Flex>
+	// 							</form>
+	// 						)}
+	// 					</Formik>
+	// 				</Box>
+	// 				<Box>
+	// 					<Text as='h3'>Results</Text>
+	// 					<Box
+	// 						width={'100%'}
+	// 						height={'100%'}
+	// 						backgroundColor={'var(--chakra-colors-blackAlpha-200)'}
+	// 						overflowY={'auto'}
+	// 						padding={'2%'}
+	// 					>{result}</Box>
+	// 					<Button mt={'2%'} onClick={() => setResult('')}>Clear</Button>
+	// 				</Box>
+	// 			</ Grid>
+	// 		</Center>
+	// 	)
+	// }
 };
 
 // export const getServerSideProps: GetServerSideProps = async (context) => {
