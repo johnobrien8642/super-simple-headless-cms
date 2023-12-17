303c321e170219bb5a099a466149c10fb6b568e1
John O'Brien <62157096+johnobrien8642@users.noreply.github.com>
2023-12-16 22:16:16 -0500

    remove unneeded models

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,8 +1,3 @@
-import Section from '../models/Section';
-import Piece from '../models/Piece';
-import Post from '../models/Post';
-import Sub from '../models/Sub';
-import Essay from '../models/Essay';
 import Admin from '../models/Admin';
 import Page from '../models/Page';
 import PageManager from '../models/PageManager';
@@ -11,11 +6,6 @@ import Assets from '../models/Assets';
 import mongoose from 'mongoose';
 
 const models: { [key: string]: typeof mongoose.Model } = {
-	Section,
-	Piece,
-	Post,
-	Sub,
-	Essay,
 	Admin,
 	Page,
 	PageManager,
