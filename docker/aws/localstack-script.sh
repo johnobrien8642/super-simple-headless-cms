cc5bce50d890b01b65382ca19d52459c6060bbee
John O'Brien <johnobrien8642@gmail.com>
2024-03-02 13:10:12 -0500

    update docker and localstack script

--- docker/aws/localstack-script.sh
+++ docker/aws/localstack-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 awslocal s3api \
-create-bucket --bucket personal-site \
+create-bucket --bucket super-simple-headless-cms \
 --create-bucket-configuration LocationConstraint=eu-central-1 \
 --region eu-central-1
-awslocal s3api put-bucket-acl --bucket personal-site --acl public-read
-awslocal s3api put-bucket-cors --bucket personal-site  --cors-configuration file://cors-config.json
+awslocal s3api put-bucket-acl --bucket super-simple-headless-cms --acl public-read
+awslocal s3api put-bucket-cors --bucket super-simple-headless-cms  --cors-configuration file://cors-config.json
